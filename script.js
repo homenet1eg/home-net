328de07d34eb7c0eef1bf70da5ea3793bb964196
homenet1eg <homenet1.eg@gmail.com>
2026-06-28 14:56:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+// Home Net V1
 
+console.log("Home Net V1 Loaded");
+
+// تأثير بسيط عند الضغط على الأزرار
+document.querySelectorAll(".btn").forEach(btn => {
+    btn.addEventListener("click", function () {
+        this.style.transform = "scale(0.96)";
+        setTimeout(() => {
+            this.style.transform = "scale(1)";
+        }, 120);
+    });
+});
