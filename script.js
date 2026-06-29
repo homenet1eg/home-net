9882d77599f10eeb44fc67330d8dd16445409363
homenet1eg <homenet1.eg@gmail.com>
2026-06-29 06:33:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,26 @@
-const offersDiv = document.getElementById("offers");
-
 function showOffers(company) {
+
+  const offersDiv = document.getElementById("offers");
 
   let data = [];
 
   if (company === "vodafone") {
     data = [
-      { title: "فودافون فليكس", text: "5200 فليكس - 125 جنيه" },
-      { title: "فودافون فليكس", text: "10400 فليكس - 230 جنيه" }
+      "فليكس 5200 - 125 جنيه",
+      "فليكس 10400 - 230 جنيه"
     ];
   }
 
   else if (company === "orange") {
     data = [
-      { title: "أورانج نت", text: "14 جيجا - 320 جنيه" },
-      { title: "أورانج نت", text: "20 جيجا - 420 جنيه" }
+      "نت 14 جيجا - 320 جنيه",
+      "نت 20 جيجا - 420 جنيه"
     ];
   }
 
-  render(data);
-}
-
-function render(data) {
   offersDiv.innerHTML = "";
 
   data.forEach(item => {
-    offersDiv.innerHTML += `
-      <div class="card">
-        <h2>${item.title}</h2>
-        <p>${item.text}</p>
-      </div>
-    `;
+    offersDiv.innerHTML += "<p>" + item + "</p>";
   });
 }
-
-// تشغيل أول ما الصفحة تفتح
-showOffers("vodafone");
